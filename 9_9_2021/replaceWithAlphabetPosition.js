56789c7211fd318bdcee3fcbe0f439d770488ecd
alex <monkeymedic26@gmail.com>
2021-09-09 15:25:04 -0700

    A solution found

--- 9_9_2021/replaceWithAlphabetPosition.js
+++ 9_9_2021/replaceWithAlphabetPosition.js
@@ -11,5 +11,24 @@
 // Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
 
 function alphabetPosition(text) {
-    return text;
+    const alphabet = [0, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    let result = "";
+    let newText = text.toLowerCase();
+    for (i = 0; i < newText.length; i++) {
+      if (result.length === 0) {
+        if (alphabet.indexOf(newText[i]) === -1) {
+          continue;
+        }
+        result += alphabet.indexOf(newText[i]).toString();
+      } else {
+        
+        if (alphabet.indexOf(newText[i]) === -1) {
+            continue;
+        }
+        result += " " + alphabet.indexOf(newText[i]).toString();
+      }
+    }
+    return result;
   }
+  
+  alphabetPosition("the");
