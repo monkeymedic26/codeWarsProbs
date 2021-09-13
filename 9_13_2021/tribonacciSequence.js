65eea6c6d1bc790dc5d5f33ccbbab3c8d61019dc
alex <monkeymedic26@gmail.com>
2021-09-13 13:17:05 -0700

    found 1 solution

--- 9_13_2021/tribonacciSequence.js
+++ 9_13_2021/tribonacciSequence.js
@@ -18,4 +18,22 @@
 
 function tribonacci(signature,n){
     //your code here
+    if (n === 0) {
+        return [];
+    }
+
+    if (n < signature.length) {
+        return signature.slice(0, n);
+    }
+
+    while (signature.length < n) {
+        let last = signature.length - 1;
+        signature.push(signature[last] + (signature[last - 1]) + (signature[last - 2]));
+    }
+    
+    return signature;
   }
+
+  tribonacci([1, 1, 1], 10); // [1,1,1,3,5,9,17,31,57,105]
+  tribonacci([1, 1, 1], 0);
+  tribonacci([1, 1, 1], 1);
