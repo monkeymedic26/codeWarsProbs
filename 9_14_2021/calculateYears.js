88dc0c4e933f1299c00a051e192c404fb71490b2
alex <monkeymedic26@gmail.com>
2021-09-14 18:32:45 -0700

    added test cases

--- 9_14_2021/calculateYears.js
+++ 9_14_2021/calculateYears.js
@@ -27,3 +27,7 @@
 function calculateYears(principal, interest, tax, desired) {
     // your code
 }
+
+calculateYears(1000, 0.05, 0.18, 1100); // 3)
+calculateYears(1000,0.01625,0.18,1200); // 14)
+calculateYears(1000,0.05,0.18,1000); // 0
